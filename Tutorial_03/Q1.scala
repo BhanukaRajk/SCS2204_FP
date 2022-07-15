d5b546e652435d5253bc6d18456f3277cb83b3fd
BhanukaRajk <bhanukayar@gmail.com>
2022-07-15 23:51:35 +0530

    Question 01 completed

---
+++ Tutorial_03/Q1.scala
@@ -0,0 +1,11 @@
+object Q1 {
+    def main(args: Array[String]) = {
+        val r: Double =  5;    //radius
+
+        print("Area of the disk = " + DiskArea(r));
+    }
+
+    def DiskArea(Radius: Double): Double = {
+        return math.Pi*Radius*Radius;
+    }
+}

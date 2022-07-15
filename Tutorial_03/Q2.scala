46e7dba389869af8c05649fb58c8fadbb57b3ffa
BhanukaRajk <bhanukayar@gmail.com>
2022-07-16 02:09:00 +0530

    Question 02 completed

--- Tutorial_03/Q2.scala
+++ Tutorial_03/Q2.scala
@@ -1,1 +1,10 @@
-object Q2 {}
+object Q2 {
+    def main(args: Array[String]) = {
+        println("Centigrade value = 35'");       //default value
+        println("Farenheit value = " + CentigradeTOFarenheit(35));
+    }
+
+    def CentigradeTOFarenheit(C :Double): Double = {
+        return C * 1.8000 + 32.00;
+    }
+}

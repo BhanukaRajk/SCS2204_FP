e820042637c8735ec2187ffb84eb02a4ee6161c0
BhanukaRajk <bhanukayar@gmail.com>
2022-07-31 13:11:49 +0530

    Question 3 completed v1

---
+++ Tutorial_04/Question3.scala
@@ -0,0 +1,24 @@
+import scala.language.implicitConversions
+
+object Question3 extends App {
+    case class Car(name: String, price: Double)
+
+    class CarUUID(car: Car) {
+        def uuid = println("car uuid = " + car.name + " - " + car.name.hashCode)
+    }
+
+    object CarExtensions {
+        implicit def uuid(car: Car): CarUUID = new CarUUID(car)
+    }
+
+    import CarExtensions._
+    
+    val cars = List(Car("bmw 3 series", 20000),
+                    Car("bmw 5 series", 50000),
+                    Car("vw passat", 10000),
+                    Car("vw golf", 12000),
+                    Car("mazda 3", 15000))
+
+    cars.foreach(car => car.uuid)
+
+}
